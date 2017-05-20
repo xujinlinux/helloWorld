4d5889ef871fe10a6a357d183732d3c0721adc57
xujinlinux <xujinlinux@163.com>
2017-05-20 21:48:46 +0800

    简单修改了
    添加了参数判断与打印

--- hello_world.c
+++ hello_world.c
@@ -3,6 +3,15 @@
 int main(int argc, char *argv[])
 {
     printf("hello world, my name is xujin, the email is xujinlinux@163.com\n");
+
+	if (argc != 2)
+	{
+		printf("param num error!\n");
+		return -1;
+	}
+
+	printf("argv[0] = %s\nargv[1] = %s\n", argv[0], argv[1]);
+	
     
     return 0;
 }
